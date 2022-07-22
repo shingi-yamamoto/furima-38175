11ce21674b7e3028a8bf99599ddf842aeb6e3bba
shingi-yamamoto <107983687+shingi-yamamoto@users.noreply.github.com>
2022-07-22 23:17:39 +0900

    ユーザー管理機能修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :users do
-    get '/users', to: redirect("/users/sign_up")
-  end
- root to: 'items#index'
+  root to: 'items#index'
 end
 
