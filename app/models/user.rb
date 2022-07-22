a3cb5259d54bdcbce50374f93e7d8ff3cbe4b597
shingi-yamamoto <107983687+shingi-yamamoto@users.noreply.github.com>
2022-07-22 19:02:50 +0900

    単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nick_name, presence: true
-  validates :email, uniqueness: true
-  validates :encrypted_password, presence: true
-  validates :family_name , presence: true
-  validates :first_name , presence: true
-  validates :family_name_kana , presence: true
-  validates :first_name_kana , presence: true
-  validates :birth_day , presence: true
+  with_options presence: true do
+    validates :nick_name
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
+    validates :family_name , format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :first_name , format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :family_name_kana , format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name_kana , format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :birth_day 
+  end
 end
