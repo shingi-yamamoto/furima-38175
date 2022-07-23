e647edf0b39432675b64cf6b58bd77e19cbdc393
shingi-yamamoto <107983687+shingi-yamamoto@users.noreply.github.com>
2022-07-24 01:10:17 +0900

    ActiveStorage導入とActiveHash導入実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,16 @@
 class Product < ApplicationRecord
-  
+
+  # テーブルとのアソシエーション
   belongs_to :user
   has_one :item_purchase
+
+  # アクティブハッシュとのアソシエーション
+  belongs_to :category_id
+  belongs_to :prefecture_id
+  belongs_to :status_id
+  belongs_to :cost_id
+  belongs_to :days_id
+
+  # active_storageとのアソシエーショ
+  has_one_attached :image
 end
