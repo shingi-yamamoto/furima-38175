477e50457fdd099213931042a1da1cdf300db13b
shingi-yamamoto <107983687+shingi-yamamoto@users.noreply.github.com>
2022-07-23 19:03:34 +0900

    ユーザー管理機能修正2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -64,6 +64,21 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
       end
+      it "全角文字を含むパスワードでは登録できない" do
+        @user.password = "あいうえお"
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password is invalid"
+      end
+      it 'パスワードが数字のみでは登録できない' do
+        @user.password = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password", "Password is invalid"
+      end
+      it 'パスワードが英語のみでは登録できない' do
+        @user.password = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password", "Password is invalid"
+      end
       it "passwordが半角英数字混合でなければ登録できない" do
       @user.password = "aaaaaa"
       @user.password_confirmation = "aaaaaa"
