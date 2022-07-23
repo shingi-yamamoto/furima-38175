67bd5d2b9a25213f608516e3208415ab886a2e2f
shingi-yamamoto <107983687+shingi-yamamoto@users.noreply.github.com>
2022-07-23 19:16:23 +0900

    ユーザー管理機能修正3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -72,12 +72,12 @@ RSpec.describe User, type: :model do
       it 'パスワードが数字のみでは登録できない' do
         @user.password = '123456'
         @user.valid?
-        expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password", "Password is invalid"
+        expect(@user.errors.full_messages).to include "Password is invalid"
       end
       it 'パスワードが英語のみでは登録できない' do
         @user.password = 'abcdef'
         @user.valid?
-        expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password", "Password is invalid"
+        expect(@user.errors.full_messages).to include "Password is invalid"
       end
       it "passwordが半角英数字混合でなければ登録できない" do
       @user.password = "aaaaaa"
